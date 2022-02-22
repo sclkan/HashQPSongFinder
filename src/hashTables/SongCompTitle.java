c4df9bf53f3ec0ebba739c0ba78a60e035685e66
Sean Kan <59797882+sclkan@users.noreply.github.com>
2022-02-23 01:56:15 +0900

    Added comments

--- src/hashTables/SongCompTitle.java
+++ src/hashTables/SongCompTitle.java
@@ -2,29 +2,61 @@ package hashTables;
 
 import cs1c.SongEntry;
 
+/**
+ * Wrapper class that gives us the ability to compare song titles
+ * @author Sean Kan
+ */
 public class SongCompTitle implements Comparable<String>
 {
     private SongEntry data;
 
+    /**
+     * Constructor with one parameter
+     * @param e  SongEntry object
+     */
     public SongCompTitle(SongEntry e)
     {
         data = e;
     }
+
+    /**
+     * Uses compareTo() to implement our find on key
+     * @param key  Key which is the song title
+     * @return Zero if the strings are identical
+     */
+    @Override
     public int compareTo(String key)
         {
             return data.getTitle().compareTo(key);
     }
+
+    /**
+     * Preserves the equals() provided by embedded data
+     * @param rhs SongCompTitle object
+     * @return True if data are equal
+     */
     public boolean equals(SongCompTitle rhs)
     {
         return data.equals(rhs.data);
     }
+
+    /**
+     *Obtains the hash code
+     * @return The hash code of the song title
+     */
+    @Override
     public int hashCode()
     {
         return data.getTitle().hashCode();
     }
+
+    /**
+     * Returns value in string format
+     * @return Output of all the elements of a SongEntry object
+     */
+    @Override
     public String toString()
     {
         return data.toString();
     }
-
 }
