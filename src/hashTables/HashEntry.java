ac87d7bf29fb627b16a3653ee29c989a3e708cb7
Sean Kan <59797882+sclkan@users.noreply.github.com>
2022-02-25 00:32:59 +0900

    Added comments

--- src/hashTables/HashEntry.java
+++ src/hashTables/HashEntry.java
@@ -1,18 +1,28 @@
 package hashTables;
 
-
-// HashEntry class for use by FHhashQP -----------------------
+/**
+ * HashEntry class for use by FHhashQP
+ * @param <E>   Generic object
+ */
 public class HashEntry<E>
 {
    public E data;
    public int state;
-   
+
+   /**
+    * Constructor that takes two parameters
+    * @param x   Generic object (SongEntry)
+    * @param st  State (Active/Empty/Deleted)
+    */
    public HashEntry( E x, int st )
    {
       data = x;
       state = st;
    }
 
+   /**
+    * Constructor with no parameters
+    */
    public HashEntry()
    {
       this(null, FHhashQP.EMPTY);
